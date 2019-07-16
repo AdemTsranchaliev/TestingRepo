f0b6efd0f381c7726bad24410d89388059810e25
AdemTsranchaliev <ademcran4aliev@abv.bg>
2019-07-16 21:23:34 +0300

    Update adem-plugin.php

--- adem-plugin.php
+++ adem-plugin.php
@@ -31,7 +31,7 @@ if( isset($_POST['demo']) )
 	       //use that way to get the information, because if i use get_content... can't escape ampersant
            $ch = curl_init();
            curl_setopt($ch, CURLOPT_URL, $url);
-           curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
+         
            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
            $result = curl_exec($ch);
            curl_close($ch);
@@ -65,7 +65,7 @@ if( isset($_POST['demo']) )
    
            $ch = curl_init();
            curl_setopt($ch, CURLOPT_URL, $url);
-           curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
+          
            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
            $resultF = curl_exec($ch);
            curl_close($ch);		 
