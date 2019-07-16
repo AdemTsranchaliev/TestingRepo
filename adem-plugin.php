9503c367686c382315ca42309f863b05a23bdac1
AdemTsranchaliev <ademcran4aliev@abv.bg>
2019-07-16 19:10:44 +0300

    Add files via upload

--- adem-plugin.php
+++ adem-plugin.php
@@ -11,38 +11,113 @@ License:     MIT
 
 defined ('ABSPATH') or die('Hey, what are you doing here? You silly human!');
 
+add_filter('the_content','GetLocationOnLoad');
+add_filter('the_content','takeFromDBtest');
+add_action('activated_plugin','GetFromApiAndSaveInDb');
 
-function takeFromDBtest(){
+
+function  takeFromDBtest(){
 	
 	global $wpdb;
+
 	
-	 $result=$wpdb->get_results("SELECT * FROM mapinfo");
+if( isset($_POST['demo']) )
+{
+   
+           $latAndLng = ($_POST['demo']);
+	       //url find the city/vilage name depens on cordinates 
+           $url = ('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$latAndLng.'&key=AIzaSyBMF1e5ozIkstmijskiXi5M5c-_yIvkQSQ');
+     
+	       //use that way to get the information, because if i use get_content... can't escape ampersant
+           $ch = curl_init();
+           curl_setopt($ch, CURLOPT_URL, $url);
+           curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
+           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+           $result = curl_exec($ch);
+           curl_close($ch);
+           		
+           $arr=[];
+           $output=(array) json_decode($result,true);
+		   echo  $output[0];
+		   
+		   //exctract city/vilage name
+           for($j=0;$j<count($output['results'][0]['address_components']);$j++)
+		   {
+               array_push($arr,$output['results'][0]['address_components'][$j]['types'][0].$output['results'][0]['address_components'][$j]['long_name']);
+           }
+		   
+		   
+	//result come as locality: city/vilage, here i take just the city name
+	$place=substr($arr[3],8); 
+
+	//removing whitespaces
+	 $place=preg_replace('/\s+/', '', $place);
 	
+	 $temp= 'SELECT * FROM mapinfo WHERE Address LIKE '.'"%'.$place.'%"';
+	echo $temp;
+
+	$results=$wpdb->get_results($temp);
+
+     //check if db have information for restaurants in city/vilage and if there is no info, i insert it
+	 if(count($results)==0)
+	 {
+		   $url = ('https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+'.$place.'&key=AIzaSyBMF1e5ozIkstmijskiXi5M5c-_yIvkQSQ');
+   
+           $ch = curl_init();
+           curl_setopt($ch, CURLOPT_URL, $url);
+           curl_setopt($ch,CURLOPT_USERAGENT,"Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13");
+           curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+           $resultF = curl_exec($ch);
+           curl_close($ch);		 
+		   //decode json
+		   $newInput=(array) json_decode($resultF,true);
+
+		   InsertIntoDatabase($newInput);
+	   
+           //get new inserted info
+		   $results=$wpdb->get_results($temp);
+
+	 }
+
+	//initialize the array with the data for map, fo JS
 	 $forRes=" var locations = [";
 	
-	foreach ( $result as $page )
+	foreach ( $results as $page )
         {
-          $forRes=$forRes."['Bondi Beach', ".(float)$page->Len.", ".(float)$page->Lng."],";
+          $forRes=$forRes."['".$page->Name."', ".(float)$page->Lat.", ".(float)$page->Lng."],";
         }
   
     $forRes=substr($forRes,0,-1)."];";
 
-    $scripts=AddScripts($forRes);
 	
-    return ("  <script src='http://maps.google.com/maps/api/js?sensor=false' type='text/javascript'></script>"."<div id='map' style='width: 500px; height: 400px;'></div> ".$scripts);
+	
+    $scripts=AddScripts($forRes,$latAndLng,13);
+	
+
+    echo ("  <script src='http://maps.google.com/maps/api/js?sensor=false' type='text/javascript'></script>"."<div id='map' style='width: 500px; height: 400px;'></div> ".$scripts);
+}
+        else
+           {
+               //initialize empty arr
+           	$forRes=" var locations = [['', '', '.']];";
+           	
+               $scripts=AddScripts($forRes,'0.0,0.0',1);
+           	
+           	$test1=GetLocationOnLoad();
+           	
+               echo ("  <script src='http://maps.google.com/maps/api/js?sensor=false' type='text/javascript'></script>"."<div id='map' style='width: 500px; height: 400px;'></div> ".$scripts.$test1);
+           	      
+           }
+
 }
 
 
 
-
-add_filter('the_content','takeFromDBtest');
-add_action('activated_plugin','GetFromApiAndSaveInDb');
-
-function AddScripts($param)
+function AddScripts($param,$latAndLng,$zoom)
 {
 	$str=  "
 
-<script type='text/javascript'>
+    <script type='text/javascript'>
     
 
 	"
@@ -52,18 +127,20 @@ function AddScripts($param)
 	"
 	
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: new google.maps.LatLng(-33.92, 151.25),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
+	 var map = new google.maps.Map(document.getElementById('map'),
+     {
+         zoom: ".$zoom.",
+         center: new google.maps.LatLng(".$latAndLng."),
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+     });
 
-    var infowindow = new google.maps.InfoWindow();
+     var infowindow = new google.maps.InfoWindow();
 
-    var marker, i;
+     var marker, i;
 
-    for (i = 0; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
+	for (i = 0; i < locations.length; i++)
+	 {  
+        marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
         map: map
       });
@@ -75,13 +152,13 @@ function AddScripts($param)
         }
       })(marker, i));
     }
-  </script>
+      </script>
 	  ";
 	  
 	  return $str;
 }
 
-
+//When plugin is installed, the function add first information in the database
 function GetFromApiAndSaveInDb($param)
 {
 	global $wpdb;
@@ -93,40 +170,89 @@ function GetFromApiAndSaveInDb($param)
 		$json = file_get_contents('https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+'.$cities[$x].'&key=YOUR_API_KEY');
 
           $arr=(array)json_decode($json,true);
-	       for ($y = 0; $y < count($arr['results']); $y++)
-	      {
-			  
-			  $name=$arr['results'][$y]['name'];
-			  $lng=float)$arr['results'][$y]['geometry']['location']['lng']
-			  $lat=(float)($arr['results'][$y]['geometry']['location']['lat'])
-			  
-			  if(CheckIfIsUnique($name,$lng,$lat))
-			  {
-				   $wpdb->insert('mapinfo',array( 'Name'=>$name,'Lng' => $lng, 'Len' =>$lat  ));
-			  }
-			  
-			  
-          echo 
-	      }
+		  InsertIntoDatabase($arr);
+	}		  
+	      
+	
+}
+
+
+function InsertIntoDatabase($newInput)
+{
+	for($j=0;$j<count($newInput['results']);$j++)
+	{
+		
+		$lat=(!is_null($newInput['results'][$j]['geometry']['location']['lat'])) ? $newInput['results'][$j]['geometry']['location']['lat'] : 0;
+		$lng=(!is_null($newInput['results'][$j]['geometry']['location']['lng'])) ? $newInput['results'][$j]['geometry']['location']['lng'] : 0;
+		$placeId=(!is_null($newInput['results'][$j]['place_id'])) ? $newInput['results'][$j]['place_id'] : '';
+		$pricingLevel=(!is_null($newInput['results'][$j]['price_level'])) ? $newInput['results'][$j]['price_level'] : 0;
+		$rating=(!is_null($newInput['results'][$j]['rating'])) ? $newInput['results'][$j]['rating'] : 0;
+		$name=(!is_null($newInput['results'][$j]['name'])) ? $newInput['results'][$j]['name'] : '';
+		$address=(!is_null($newInput['results'][$j]['formatted_address'])) ? $newInput['results'][$j]['formatted_address'] : '';	
+
+		if(CheckIfIsUnique($name,$lat,$lng))
+		{
+			echo  $wpdb->insert('mapinfo',array( 'Name'=>$name,'Lat' => $lat, 'Lng' =>$lng,'PlaceId' => $placeId, 'PricingLevel' =>$pricingLevel, 'Rating' => $rating, 'Address' => $address  ));
+		}
+		
 	}
-    }
+}
     
 	
-	function bool CheckIfIsUnique($name,$lng,$lat)
+function CheckIfIsUnique($name,$lng,$lat)
+{
+	global $wpdb;
+	
+	$results = $wpdb->get_results( "SELECT * FROM mapinfo  WHERE Name=".$name." AND lng=".lng." AND len=".$lat );
+	
+	if(is_null($results))
 	{
-		global $wpdb;
-		
-		$results = $wpdb->get_results( "SELECT * FROM mapinfo  WHERE Name=".$name." AND lng=".lng." AND len=".$lat );
-		if(is_null($results))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return true;
 	}
-  
+	else
+	{
+		return false;
+	}
+}
+   
+function GetLocationOnLoad()
+{
+	return "	
+	
+ <form  name='form123' id='form123' method='post'>
+ <input type='hidden' id='demo' name='demo' />
+ </form>
+		  
+ 
+ <script type='text/javascript'>
+
+ var x=document.getElementById('demo');
+
+ function getLocation()
+ {
+	if (navigator.geolocation)
+	{
+		navigator.geolocation.getCurrentPosition(showPosition);
+	}
+	else
+	{
+		x.value='Geolocation is not supported by this browser.';
+	}
+ }
+ function showPosition(position)
+ {
+		 x.value=position.coords.latitude + ',' + position.coords.longitude;  
+	 
+		 document.getElementById('form123').submit();
+ }
+ 
+
+ getLocation();
+
+ </script>";
+ 
+ 
+}
 
 
 
