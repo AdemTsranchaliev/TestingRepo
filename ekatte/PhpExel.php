412907ee3f446a515a8a9c1af7b23251ec5f28e9
Adem <ademcran4aliev@abv.bg>
2019-09-05 18:29:10 +0300

    Commit remove

--- ekatte/PhpExel.php
+++ ekatte/PhpExel.php
@@ -3,87 +3,87 @@
 require 'vendor/autoload.php';
 
 
-//  function addToDatabase($query)
-//  {
-//      $db = pg_connect("host=localhost port=5433 dbname=Bulgaria2 user=postgres password=34523452");
+function addToDatabase($query)
+{
+    $db = pg_connect("host=localhost port=5433 dbname=Bulgaria2 user=postgres password=34523452");
 
-//  if(!$db)
-//  {
-//      echo "Изникна грешка по време на свързването с базата, моля опитайте отново";
-//      return;
-//  }
-//      $result = pg_query($query); 
-//      if(!$result)
-//    {
-//        echo "Изникна грешка по време на записването на данните, моля проверете какви са данните, които въвеждате";
-//        return;
-//    }
-
-
-//  }
+if(!$db)
+{
+    echo "Изникна грешка по време на свързването с базата, моля опитайте отново";
+    return;
+}
+    $result = pg_query($query); 
+    if(!$result)
+  {
+      echo "Изникна грешка по време на записването на данните, моля проверете какви са данните, които въвеждате";
+      return;
+  }
 
 
-//    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-//    $reader->setReadDataOnly(true);
-//    $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_obl.xlsx");
+}
 
 
-//    $arr1=$spreadsheet->getActiveSheet()->toArray();
+  $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+  $reader->setReadDataOnly(true);
+  $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_obl.xlsx");
 
 
-
-//    $query1 = 'INSERT INTO regions(name,id) VALUES';
-//    for ($i=1; $i < count($arr1); $i++) { 
-//          $query1=$query1."('".$arr1[$i][2]."','".$arr1[$i][0]."'),";   
-//    }
-
-//      $query1=substr($query1,0,-1).';';
-//      addToDatabase($query1);
-
-  
-//    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-//    $reader->setReadDataOnly(true);
-//    $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_obst.xlsx");
-
-
-//    $arr1=$spreadsheet->getActiveSheet()->toArray();
-
-//      $query1 = 'INSERT INTO municipalities(name,id,region_id,category) VALUES';
-//      for ($i=1; $i < count($arr1); $i++) { 
-//            $query1=$query1."('".$arr1[$i][2]."','".$arr1[$i][0]."','".substr($arr1 [$i][0],0,3)."',".$arr1[$i][3]."),";   
-//        }
-   
-
-//        $query1=substr($query1,0,-1).';';
-//        addToDatabase($query1);
-
-  
-    
-  // $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-  //   $reader->setReadDataOnly(true);
-  //   $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_atte.xlsx");
-
-
-  //   $arr1=$spreadsheet->getActiveSheet()->toArray();
+  $arr1=$spreadsheet->getActiveSheet()->toArray();
 
 
 
-  //      $query1 = 'INSERT INTO allsettlements(ekatte,altitude,kind,category,municipality_id,name) VALUES';
-  //      for ($i=2; $i < count($arr1); $i++) { 
-  //            if($arr1[$i][8]=='')
-  //            {
-  //              $arr1[$i][8]='NULL'; 
-  //            }
-  //            if($arr1[$i][7]=='')
-  //            {
-  //              $arr1[$i][7]='NULL'; 
-  //            }
-  //            $query1=$query1."('".$arr1[$i][0]."',".$arr1[$i][8].",'".$arr1[$i][1]."',".$arr1[$i][7].",'".$arr1[$i][4]."','".$arr1[$i][2]."' ),";   
-   
-  //      }
+  $query1 = 'INSERT INTO regions(name,id) VALUES';
+  for ($i=1; $i < count($arr1); $i++) { 
+        $query1=$query1."('".$arr1[$i][2]."','".$arr1[$i][0]."'),";   
+  }
 
-  //        $query1=substr($query1,0,-1).';';
-  //        addToDatabase($query1);
+    $query1=substr($query1,0,-1).';';
+    addToDatabase($query1);
+
+
+  $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+  $reader->setReadDataOnly(true);
+  $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_obst.xlsx");
+
+
+  $arr1=$spreadsheet->getActiveSheet()->toArray();
+
+    $query1 = 'INSERT INTO municipalities(name,id,region_id,category) VALUES';
+    for ($i=1; $i < count($arr1); $i++) { 
+          $query1=$query1."('".$arr1[$i][2]."','".$arr1[$i][0]."','".substr($arr1 [$i][0],0,3)."',".$arr1[$i][3]."),";   
+      }
+
+
+      $query1=substr($query1,0,-1).';';
+      addToDatabase($query1);
+
+
+
+ $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+   $reader->setReadDataOnly(true);
+   $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_atte.xlsx");
+
+
+   $arr1=$spreadsheet->getActiveSheet()->toArray();
+
+
+
+      $query1 = 'INSERT INTO allsettlements(ekatte,altitude,kind,category,municipality_id,name) VALUES';
+      for ($i=2; $i < count($arr1); $i++) { 
+            if($arr1[$i][8]=='')
+            {
+              $arr1[$i][8]='NULL'; 
+            }
+            if($arr1[$i][7]=='')
+            {
+              $arr1[$i][7]='NULL'; 
+            }
+            $query1=$query1."('".$arr1[$i][0]."',".$arr1[$i][8].",'".$arr1[$i][1]."',".$arr1[$i][7].",'".$arr1[$i][4]."','".$arr1[$i][2]."' ),";   
+
+      }
+
+        $query1=substr($query1,0,-1).';';
+        addToDatabase($query1);
 
 
 
