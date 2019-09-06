63395721df76921d487cbd8b3ea5d6cf549ec918
Adem <ademcran4aliev@abv.bg>
2019-09-06 10:43:52 +0300

    Improving

--- ekatte/PhpExel.php
+++ ekatte/PhpExel.php
@@ -2,60 +2,144 @@
 
 require 'vendor/autoload.php';
 
+echo "<style>
 
-function addToDatabase($query)
+button {
+
+background-color: blue;
+height: 40px;
+width: 30%;
+}
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+  background-color: #cc0000;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>";
+
+function htmlError($ser)
 {
+    $str="
+<!DOCTYPE html>
+
+
+
+<table>
+  <tr>
+    <th>".$ser." не беше намерено!</th>
+  </tr>
+</table>
+
+";
+
+return $str;
+}
+
+
+echo "<a href='http://localhost:8022/settlemets/index.html'><button>Към търсачка</button></a>";
+
+
+   $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+   $reader->setReadDataOnly(true);
+   $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_obl.xlsx");
+
+
+   $arr1=$spreadsheet->getActiveSheet()->toArray();
+
+
+
+   $query1 = 'INSERT INTO regions(name,id) VALUES';
+   for ($i=1; $i < count($arr1); $i++) { 
+
     $db = pg_connect("host=localhost port=5433 dbname=Bulgaria2 user=postgres password=34523452");
 
-if(!$db)
-{
-    echo "Изникна грешка по време на свързването с базата, моля опитайте отново";
-    return;
-}
-    $result = pg_query($query); 
-    if(!$result)
-  {
-      echo "Изникна грешка по време на записването на данните, моля проверете какви са данните, които въвеждате";
-      return;
-  }
+    if(!$db)
+    {
+        echo "Изникна грешка по време на свързването с базата, моля опитайте отново";
+        return;
+    }
+ 
+      $result = pg_query("SELECT * FROM regions WHERE id='".$arr1[$i][0]."' OR name='".$arr1[$i][2]."'"); 
+      $res=FALSE;
 
+      while ($row = pg_fetch_row($result)) {
+   
+         $res=TRUE;
+       echo  htmlError("Вашият запис с id: '".$arr1[$i][0]."' не можа да бъде записан в базата regions, понеже съществува друг с това id или name");
+     }
+   
+      if(!$res)
+         {
+      $query1=$query1."('".$arr1[$i][2]."','".$arr1[$i][0]."'),"; 
+         }  
+   }
 
-}
+    
 
-
-  $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-  $reader->setReadDataOnly(true);
-  $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_obl.xlsx");
-
-
-  $arr1=$spreadsheet->getActiveSheet()->toArray();
-
-
-
-  $query1 = 'INSERT INTO regions(name,id) VALUES';
-  for ($i=1; $i < count($arr1); $i++) { 
-        $query1=$query1."('".$arr1[$i][2]."','".$arr1[$i][0]."'),";   
-  }
-
-    $query1=substr($query1,0,-1).';';
-    addToDatabase($query1);
-
-
-  $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-  $reader->setReadDataOnly(true);
-  $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_obst.xlsx");
-
-
-  $arr1=$spreadsheet->getActiveSheet()->toArray();
-
-    $query1 = 'INSERT INTO municipalities(name,id,region_id,category) VALUES';
-    for ($i=1; $i < count($arr1); $i++) { 
-          $query1=$query1."('".$arr1[$i][2]."','".$arr1[$i][0]."','".substr($arr1 [$i][0],0,3)."',".$arr1[$i][3]."),";   
-      }
-
-
+     if ('INSERT INTO regions(name,id) VALUES'!=$query1) {
       $query1=substr($query1,0,-1).';';
-      addToDatabase($query1);
+      pg_query($query1); 
+     }
+    
+
+
+   $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+   $reader->setReadDataOnly(true);
+   $spreadsheet = $reader->load("C:/Users/Asus/Desktop/Ekatte/ek_obst.xlsx");
+
+
+   $arr1=$spreadsheet->getActiveSheet()->toArray();
+
+     $query1 = 'INSERT INTO municipalities(name,id,region_id,category) VALUES';
+     for ($i=1; $i < count($arr1); $i++) { 
+
+
+      $db = pg_connect("host=localhost port=5433 dbname=Bulgaria2 user=postgres password=34523452");
+
+      if(!$db)
+      {
+          echo "Изникна грешка по време на свързването с базата, моля опитайте отново";
+          return;
+      }
+   
+        $result = pg_query("SELECT * FROM municipalities WHERE id='".$arr1[$i][0]."'"); 
+        $res=FALSE;
+  
+        while ($row = pg_fetch_row($result)) {
+     
+           $res=TRUE;
+         echo  htmlError("Вашият запис с id: '".$arr1[$i][0]."' не можа да бъде записан в базата municipalities, понеже съществува друг с това id или name");
+       }
+     
+        if(!$res)
+           {
+        
+            $query1=$query1."('".$arr1[$i][2]."','".$arr1[$i][0]."','".substr($arr1 [$i][0],0,3)."',".$arr1[$i][3]."),"; 
+           }  
+
+
+  
+       }
+
+
+      
+       if ('INSERT INTO municipalities(name,id,region_id,category) VALUES'!=$query1) {
+        $query1=substr($query1,0,-1).';';
+        pg_query($query1); 
+       }
+       
+
 
 
 
@@ -68,8 +152,44 @@ if(!$db)
 
 
 
-      $query1 = 'INSERT INTO allsettlements(ekatte,altitude,kind,category,municipality_id,name) VALUES';
-      for ($i=2; $i < count($arr1); $i++) { 
+   $query1 = 'INSERT INTO allsettlements(ekatte,altitude,kind,category,municipality_id,name) VALUES';
+     $res=FALSE;
+   for ($i=2; $i < count($arr1); $i++) { 
+
+        $db = pg_connect("host=localhost port=5433 dbname=Bulgaria2 user=postgres password=34523452");
+
+        if(!$db)
+        {
+            echo "Изникна грешка по време на свързването с базата, моля опитайте отново";
+            return;
+        }
+     
+          $result = pg_query("SELECT * FROM allsettlements WHERE ekatte='".$arr1[$i][0]."'"); 
+          $res=FALSE;
+
+          while ($row = pg_fetch_row($result)) {
+       
+             $res=TRUE;
+           echo  htmlError("Вашият запис с ekatte: '".$arr1[$i][0]."' не можа да бъде записан, понеже съществува друг с това ekatte");
+         }
+       
+                $QR="SELECT * FROM allsettlements WHERE name='".$arr1[$i][2]."' AND kind='".$arr1[$i][1]."' AND municipaLIty_id='".$arr1[$i][4]."'";
+        
+          $result = pg_query($QR); 
+       
+          while ($row = pg_fetch_row($result)) {
+            $res=TRUE;
+          
+            echo htmlError("Вашият запис с ekatte: '".$arr1[$i][0]."' не можа да бъде записан, понеже съществува друг с това име");
+         }
+         
+          if(!$result)
+          {
+              echo "Изникна грешка по време на записването на данните, моля проверете какви са данните, които въвеждате";
+              return;
+          }
+          
+
             if($arr1[$i][8]=='')
             {
               $arr1[$i][8]='NULL'; 
@@ -79,16 +199,14 @@ if(!$db)
               $arr1[$i][7]='NULL'; 
             }
             $query1=$query1."('".$arr1[$i][0]."',".$arr1[$i][8].",'".$arr1[$i][1]."',".$arr1[$i][7].",'".$arr1[$i][4]."','".$arr1[$i][2]."' ),";   
-
+         
       }
-
-        $query1=substr($query1,0,-1).';';
-        addToDatabase($query1);
-
-
-
-
-
+  
+       pg_connect("host=localhost port=5433 dbname=Bulgaria2 user=postgres password=34523452");
+      if ($query1!='INSERT INTO allsettlements(ekatte,altitude,kind,category,municipality_id,name) VALUES') {
+        $query1=substr($query1,0,-1);
+        pg_query($query1);
+      }
       $myfile = fopen("allsettlements.txt", "w") or die("Unable to open file!");
 
 
@@ -118,13 +236,15 @@ $myfile1 = fopen("index.html", "r+") or die("Unable to open file!");
 $text=fread($myfile,filesize("allsettlements.txt"));
 $text1=fread($myfile1,filesize("index.html"));
 $num= strrpos($text1, 'var countries = [', 0);
-$num2=strrpos($text1, '"];', $num);
+$num2=strrpos($text1, '];', $num);
 
 $lat=substr($text1,$num,$num2-$num+3);
+
 $res= str_replace($lat,$text,$text1);
-fclose($myfile);
-fclose($myfile1);
-$myfile22 = fopen("index.html", "w") or die("Unable to open file!");
+
+ fclose($myfile);
+ fclose($myfile1);
+ $myfile22 = fopen("index.html", "w") or die("Unable to open file!");
  fwrite($myfile22,$res);
  fclose($myfile22);
 
