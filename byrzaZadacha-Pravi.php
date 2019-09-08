739d06e7fd06c741583473337ef2bb835b962769
AdemTsranchaliev <ademcran4aliev@abv.bg>
2019-09-08 18:57:28 +0300

    Update byrzaZadacha-Pravi.php

--- byrzaZadacha-Pravi.php
+++ byrzaZadacha-Pravi.php
@@ -8,16 +8,18 @@ $a=$_POST['a'];
 $b=$_POST['b'];
 $c=$_POST['c'];
 $arr=[];
+$arr2=[];
 $counter=0;
 for ($i=0; $i <=$n; $i++) { 
     $arr[$i]=0;
+    $arr2[$i]=0;
 }
 
 for ($i=0; $i <=$n; $i+=$a) { 
    $arr[$i]=1;
 }
 
-for ($i=$n-$b; $i>= 1;$i-=$b) { 
+for ($i=$n; $i>= 1;$i-=$b) { 
 
     if ($arr[$i]==1) {
         $arr[$i]=3;
@@ -29,22 +31,45 @@ for ($i=$n-$b; $i>= 1;$i-=$b) {
 for ($i=0; $i < $n; $i++) { 
     if ($arr[$i]==1) {
         if ($arr[$i+$c]==2||$arr[$i+$c]==3) {
-            $counter+=$c;
+            for ($j=$i+1; $j <= $i+$c; $j++) { 
+                if ($arr2[$j]==0) {
+                    $arr2[$j]=1;
+                }
+            }
         }
     }
     else if($arr[$i]==2)
     {
         if ($arr[$i+$c]==1||$arr[$i+$c]==3) {
-            $counter+=$c;
+            for ($j=$i+1; $j <= $i+$c; $j++) { 
+                if ($arr2[$j]==0) {
+                    $arr2[$j]=1;
+                }
+            }
         }
     }
     else if($arr[$i]==3)
     {
-        if ($arr[$i+$c]==1||$arr[$i+$c]==2) {
-            $counter+=$c;
+        if ($i+$c<=20) {
+            if ($arr[$i+$c]==1||$arr[$i+$c]==2) {
+                for ($j=$i+1; $j <= $i+$c; $j++) { 
+                    if ($arr2[$j]==0) {
+                        $arr2[$j]=1;
+                    }
+                }
+            }
         }
+      
     }  
 }
+for ($i=0; $i < $n; $i++) { 
+    echo $arr2[$i];
+    if ($arr2[$i]==1) {
+        
+        $counter++;
+    }
+}
+echo '<br/>';
 echo $n-$counter;
 }
 else{
